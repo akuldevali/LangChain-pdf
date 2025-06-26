f4f086e754f57411de477902cbc6770650f2de38
Akul G <akulg005@gmail.com>
2025-06-25 20:41:10 -0400

    Fixing the CondenseQuestionChain to use the right model/handler by passing a different chat_model with streaming set to false also modifying the handler to ignore false flag events

--- app/chat/chains/streamable.py
+++ app/chat/chains/streamable.py
@@ -1,7 +1,7 @@
 from queue import Queue
 from threading import Thread
 from app.chat.callbacks.stream import StreamingHandler
-
+from flask import current_app
 
 
 class StreamableChain:
@@ -14,10 +14,13 @@ class StreamableChain:
         # self(input)  This executes the chian but waits for all tokens to be assembled before executing next line, which defeats our purpuse of streaming from 
         #Can solve this by running it in a separate thread
 
-        def task():
-            self(input, callbacks=[handler]) #adding callbacks here from line31 so that
+        def task(app_context):
+            app_context.push()
+            self(input, callbacks=[handler]) #We are defining our handler and passing into the chain at call time. i.e whenevr we call Chain, we pass handler and it gets used by all objects inside the chain
 
-        Thread(target=task).start()
+        #Sadly this thread wont have access to application context i.e Flask error
+        #Hence we pass context as args when we call a new thread. i.e task()
+        Thread(target=task, args=[current_app.app_context()]).start()
 
         while True:
             token = queue.get()
--- app/chat/chat.py
+++ app/chat/chat.py
@@ -4,6 +4,7 @@ from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llms.chatopenai import build_llm
 from app.chat.memories.sql_memory import build_memory
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
+from langchain_community.chat_models import ChatOpenAI
 
 def build_chat(chat_args: ChatArgs):
     """
@@ -19,10 +20,13 @@ def build_chat(chat_args: ChatArgs):
 
     retriever = build_retriever(chat_args)
     llm = build_llm(chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False) #Specifically used by condense_ques_chain
     memory = build_memory(chat_args)
 
+    #modified the from_llm src code to accomodate 2 different chat_models
     return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
+        condense_question_llm=condense_question_llm,
         memory=memory,
         retriever=retriever
     )
