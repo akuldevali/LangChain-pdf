cc79febaf53c3557418f064d247b3bb1106c66f7
Akul G <akulg005@gmail.com>
2025-06-29 19:49:36 -0400

    Implementing window memory before langfuse integration

--- app/chat/memories/window_memory.py
+++ app/chat/memories/window_memory.py
@@ -1,12 +1,11 @@
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
 from app.chat.memories.histories.sql_history import SqlMessageHistory
 
-
-
-def build_memory(chat_args):
-    return ConversationBufferMemory(
+def window_buffer_memory_builder(chat_args):
+    return ConversationBufferWindowMemory(
         chat_memory=SqlMessageHistory(convo_id=chat_args.conversation_id),
         return_messages=True,
         memory_key="chat_history",
-        output_key="answer"
+        output_key="answer",
+        k=2
     )
